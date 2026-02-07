66084e27d617768b4bf5bc24a87b2d9edb5fc161
BunyawatCharnwechsart <bunyawat2149m@gmail.com>
2026-02-07 13:31:26 +0700

    setup pages

--- frontend-easycook/app/pages/login.vue
+++ frontend-easycook/app/pages/login.vue
@@ -1,5 +1,22 @@
+<script setup>
+const config = useRuntimeConfig()
+
+const users = await $fetch(`${config.public.apiBase}/users`)
+</script>
+
 <template>
-    <div>
-        <h2>Login</h2>
+    <div class="bg-amber-400 p-4">
+        <h1 class="text-xl font-bold mb-2">Nuxt + Hono This is Login page</h1>
+        <ul>
+        <li
+            v-for="u in users"
+            :key="u.uid"
+            class="bg-white mb-2 p-2 rounded"
+        >
+            <p>ชื่อ: {{ u.name }}</p>
+            <p>Email: {{ u.email }}</p>
+        </li>
+        </ul>
+        <nuxt-link to="/register">register</nuxt-link>
     </div>
 </template>
