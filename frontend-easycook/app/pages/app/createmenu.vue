e98958e9526c7a2fb38ed048b6f70a7275e40329
fahsai <phattharaphontangtaeng718@gmail.com>
2026-03-08 14:15:40 +0700

    ResetPassword This feature allows users who have forgotten their password to set a new password.

--- frontend-easycook/app/pages/app/createmenu.vue
+++ frontend-easycook/app/pages/app/createmenu.vue
@@ -2,7 +2,11 @@
     <div style="font-family: 'Bai Jamjuree'">
         <navBar/>
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         create Menu
+=======
+                   
+>>>>>>> Stashed changes
 =======
                    
 >>>>>>> Stashed changes
