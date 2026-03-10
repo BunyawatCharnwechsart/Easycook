6b4294aff35c649c0385d280ecbefcf3fd1444be
BunyawatCharnwechsart <bunyawat2149m@gmail.com>
2026-03-11 01:48:09 +0700

    Update main.vue

--- frontend-easycook/app/pages/app/main.vue
+++ frontend-easycook/app/pages/app/main.vue
@@ -61,7 +61,7 @@
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import navBar from '~/components/navBar.vue';
-import aboutFooter from '~/components/AboutFooter.vue';
+import AboutFooter from '~/components/AboutFooter.vue';
 import ADS from '~/components/ADS.vue';
 import RecommendedSection from '~/components/RecommendedSection.vue';
 import RecommendedMenu from '~/components/RecommendedMenu.vue';
